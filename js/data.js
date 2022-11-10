521b8aa3be1de13fceeba860badad1d944c6f819
Set Paing <setpaing20022002@gmail.com>
2022-11-10 00:32:31 -0800

    colorsss-and-some-animation

--- js/data.js
+++ js/data.js
@@ -2,15 +2,15 @@ var data = [{
     "course": "MATH-101B-05",
     "prof": "Atique N.",
     "finalDate": "Dec 12 2022 15:00:00 PST",
-    "color": [155, 50, 255]
+    "color": [125, 100, 255]
 }, {
     "course": "PHYS-140-02",
     "prof": "Desmedt L.",
     "finalDate": "Dec 15 2022 8:00:00 PST",
-    "color": [255, 80, 20]
+    "color": [250, 80, 60]
 }, {
     "course": "PHYS-140-03",
     "prof": "Desmedt L.",
     "finalDate": "Dec 12 2022 11:00:00 PST",
-    "color": [50, 105, 150]
+    "color": [0, 125, 180]
 }]
