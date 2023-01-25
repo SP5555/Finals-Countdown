17fc5bd794f24cdca91e5d3d6147f92608b78e7a
Set Paing <setpaing20022002@gmail.com>
2023-01-25 00:01:21 -0800

    23SP-update

--- js/data.js
+++ js/data.js
@@ -1,9 +1,13 @@
 var data = [{
-    "course": "Grade Release",
-    "prof": "check MyOhlone",
-    "finalDate": "Dec 22 2022 00:00:00 PST"
+    "course": "PHYS-141-02",
+    "prof": "Misra R.",
+    "finalDate": "May 15 2023 15:00:00 PDT"
 },{
-    "course": "Spring 2023",
-    "prof": "Jan 23",
-    "finalDate": "Jan 23 2023 00:00:00 PST"
+    "course": "MATH-101C-01",
+    "prof": "Nguyen A.",
+    "finalDate": "May 17 2023 11:00:00 PDT"
+},{
+    "course": "CS-124-01",
+    "prof": "Chenhansa S.",
+    "finalDate": "May 18 2023 08:00:00 PDT"
 }]
